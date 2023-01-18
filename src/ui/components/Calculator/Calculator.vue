d620369263864e4dc08235981e210dd8d139cbd2
catakkio <>
2023-01-18 10:58:29 +0100

    no select class added

--- src/ui/components/Calculator/Calculator.vue
+++ src/ui/components/Calculator/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="calculator-container">
+    <div class="calculator-container noselect">
       <div class="w-full h-full">
         <calculator-display class="h-1/5" data-testid="calculator-display" />
         <calculator-keyboard data-testid="calculator-keyboard" />
@@ -29,5 +29,14 @@ export default {
   height: 700px;
   width: 410px;
   border: 1px solid gray;
+}
+
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none;
 }
 </style>
